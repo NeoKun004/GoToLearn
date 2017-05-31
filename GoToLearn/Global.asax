d693bb73a736eda51df378bb3b7921fa175c2a2f
Hamdouch_hamidou <Hamdouch_hamidou@HAMDI_PC>
2017-05-31 17:47:30 +0100

    Add project files.

---
+++ GoToLearn/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GoToLearn.MvcApplication" Language="C#" %>
---
+++ GoToLearn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GoToLearn.Startup))]
+namespace GoToLearn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
